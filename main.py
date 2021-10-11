3c98e697e268c337375fad018271fa9f44b40834
022by <46434255+pplulee@users.noreply.github.com>
2021-10-11 16:46:24 +0800

    把签到按钮的name抓了，可以正常运行

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import schedule
 opt = webdriver.ChromeOptions()
 driver = webdriver.Chrome(options=opt)
 
-#第一行加username，第二行加password
+# 第一行加username，第二行加password
 details = open('account.txt').read().splitlines()
 
 
@@ -25,23 +25,31 @@ class user:
 
     def refresh(self):
         print("打开URL")
-        driver.get('https://my.manchester.ac.uk/en/MyCheckIn')
+        driver.get('https://my.manchester.ac.uk/MyCheckIn')
         try:
-            driver.find_element_by_class_name('c-button') # 检测登出按钮
+            driver.find_element_by_class_name('c-button')  # 检测登出按钮
             print("已登录，状态正常")
         except BaseException:
             print("未登陆，开始登陆")
             self.login()
-            
+
     def checkin(self):
         self.refresh()
         try:
-            driver.find_element_by_id('').click()  # 还没抓  
+            driver.find_element_by_name('StudentSelfCheckinSubmit').click()  # 还没抓
         except BaseException:
             print("未检测到需要签到的项目")
-            
+
+    def getcheckintime(self):
+        self.refresh()
+        try:
+            content = driver.find_element_by_xpath("//*[contains(text(),'Check-in open at ')]").text  # 还没抓
+        except BaseException:
+            print("未检测到需要签到的项目")
+        else:
+            return content[-5:]  # 首个任务的时间
 
 
 userobj = user(details[0], details[1])
 userobj.checkin()
-#schedule.every().hour.at(":52").do(userobj.signin())
+# schedule.every().hour.at(":52").do(userobj.signin())
