6664b6ef30b2f7788b3a8f17262e1c9b5b0dc4ef
022by <46434255+pplulee@users.noreply.github.com>
2022-10-03 21:33:58 +0100

    修改webdriver参数

--- main.py
+++ main.py
@@ -163,7 +163,11 @@ def setup_driver():
     options.add_argument("--headless")
     options.add_argument("--disable-gpu")
     options.add_argument("--disable-dev-shm-usage")
-    options.add_argument('ignore-certificate-errors')
+    options.add_argument("--ignore-certificate-errors")
+    options.add_argument("enable-automation")
+    options.add_argument("--disable-extensions")
+    options.add_argument("start-maximized")
+    options.add_argument("window-size=1920,1080")
     options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
                          "Chrome/101.0.4951.54 Safari/537.36")
     try:
